de9fa8038333685b898fcdb0c050c58f092c5400
Krzysiek526 <filipkrzys526@gmail.com>
2020-07-14 11:37:35 +0200

    Updates

---
+++ precache-manifest.1cefe2a16a38ce1b3e349e2e62fe41b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765e5e56a256cbb69a1ed6a97797ea75",
+    "url": "/projectR/index.html"
+  },
+  {
+    "revision": "9e14fda9577108616348",
+    "url": "/projectR/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2fd9bd684a5f3dc20ba5",
+    "url": "/projectR/static/css/main.25e43ea4.chunk.css"
+  },
+  {
+    "revision": "9e14fda9577108616348",
+    "url": "/projectR/static/js/2.a11c807c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projectR/static/js/2.a11c807c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd9bd684a5f3dc20ba5",
+    "url": "/projectR/static/js/main.88ec0c7c.chunk.js"
+  },
+  {
+    "revision": "adab00cd3459e316b462",
+    "url": "/projectR/static/js/runtime-main.b56af927.js"
+  }
+]);
